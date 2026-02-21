27dd6a441334afce9e64c89d37ed8589e24a6c38
Neha Himesh <nehahimesh.10@gmail.com>
2026-02-21 18:20:57 +0530

    Episode 4's props

--- App.js
+++ App.js
@@ -1,19 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const JsxHeading = () => (
-  <h1 className="namaste-heading" tabIndex="8">
-    Namaste React using JSX
-  </h1>
-);
-console.log(JsxHeading);
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const HeadingComponent = () => (
-  <div className="container">
-    <JsxHeading />
-    <h1> Namaste React Functional Component </h1>
-  </div>
-);
+const RestaurantCard = (props) => {
+  console.log(props);
+  return (
+    <div className="restaurant-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="restaurant-logo"
+        alt="restaurant-logo"
+        src="https://imgmedia.lbb.in/media/2018/12/5c02f558b2944367333a7da7_1543697752379.jpg?w=1200&h=628&fill=blur&fit=fill"
+      />
+      <h3>{props.resName}</h3>
+      <h4>{props.cuisine}</h4>
+      <h4>4.5 ⭐</h4>
+      <h4>38 min</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restaurant-container">
+        <RestaurantCard
+          resName="Meghana Foods"
+          cuisine="Biryani, North Indian, Asian"
+        />
+        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
