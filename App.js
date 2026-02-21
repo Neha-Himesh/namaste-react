4b88144e662f62f4ff7e060bbd64787976b6238e
Neha Himesh <nehahimesh.10@gmail.com>
2026-02-21 13:39:52 +0530

    Episode 3 code

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
+const JsxHeading = () => (
+  <h1 className="namaste-heading" tabIndex="8">
+    Namaste React using JSX
+  </h1>
+);
+console.log(JsxHeading);
+
+const HeadingComponent = () => (
+  <div className="container">
+    <JsxHeading />
+    <h1> Namaste React Functional Component </h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
