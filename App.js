351ee1953a1168aab1c85fe8ebf022e228c6b427
Neha Himesh <nehahimesh.10@gmail.com>
2026-02-22 01:12:42 +0530

    Added Swiggy API's single object and called the same using props

--- App.js
+++ App.js
@@ -23,31 +23,98 @@ const Header = () => {
 };
 
 const RestaurantCard = (props) => {
-  const { resName, cuisine } = props;
+  const { resData } = props;
   return (
     <div className="restaurant-card" style={{ backgroundColor: "#f0f0f0" }}>
       <img
         className="restaurant-logo"
         alt="restaurant-logo"
-        src="https://imgmedia.lbb.in/media/2018/12/5c02f558b2944367333a7da7_1543697752379.jpg?w=1200&h=628&fill=blur&fit=fill"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          resData.info.cloudinaryImageId
+        }
       />
-      <h3>{resName}</h3>
-      <h4>{cuisine}</h4>
-      <h4>4.5 ⭐</h4>
-      <h4>38 min</h4>
+      <h3>{resData.info.name}</h3>
+      <h4>{resData.info.cuisines.join(", ")}</h4>
+      <h4>{resData.info.avgRating}</h4>
+      <h4>{resData.info.costForTwo}</h4>
+      <h4>{resData.info.sla.deliveryTime} mins</h4>
     </div>
   );
+};
+const resObj = {
+  info: {
+    id: "621512",
+    name: "The Belgian Waffle Co.",
+    cloudinaryImageId:
+      "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/16/29ddfffe-7016-4548-ae60-1a8f348ffbe6_621512.jpg",
+    locality: "K.H.B Colony",
+    areaName: "Koramangala",
+    costForTwo: "₹200 for two",
+    cuisines: ["Waffle", "Desserts", "Ice Cream", "Beverages"],
+    avgRating: 4.6,
+    veg: true,
+    parentId: "2233",
+    avgRatingString: "4.6",
+    totalRatingsString: "2.4K+",
+    sla: {
+      deliveryTime: 23,
+      lastMileTravel: 1.4,
+      serviceability: "SERVICEABLE",
+      slaString: "20-25 mins",
+      lastMileTravelString: "1.4 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2026-02-22 01:00:00",
+      opened: true,
+    },
+    badges: {},
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {},
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    aggregatedDiscountInfoV3: {
+      header: "₹150 OFF",
+      subHeader: "ABOVE ₹999",
+      discountTag: "FLAT DEAL",
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+    externalRatings: {
+      aggregatedRating: {
+        rating: "--",
+      },
+    },
+    ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+  },
+  analytics: {
+    context: "seo-data-e24f416e-46d1-4197-9d30-5acbb1ce738e",
+  },
+  cta: {
+    link: "https://www.swiggy.com/city/bangalore/the-belgian-waffle-co-k-h-b-colony-koramangala-rest621512",
+    type: "WEBLINK",
+  },
 };
 const Body = () => {
   return (
     <div className="body">
       <div className="search">Search</div>
       <div className="restaurant-container">
-        <RestaurantCard
-          resName="Meghana Foods"
-          cuisine="Biryani, North Indian, Asian"
-        />
-        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
+        <RestaurantCard resData={resObj} />
       </div>
     </div>
   );
