8b278076a62db96599d5d740c21aeb6d9a1ed20a
Neha Himesh <nehahimesh.10@gmail.com>
2026-02-24 00:10:13 +0530

    Attempt to retrieve the restaurant cards without hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,18 @@ import resList from "../utils/mockData";
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() =>
+            (resList = resList.filter(
+              (restaurant) => restaurant.info.avgRating > 4,
+            ))
+          }
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="restaurant-container">
         {resList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
