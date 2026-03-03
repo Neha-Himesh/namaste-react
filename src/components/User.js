30aea745e5c06399c749c195c0a64eb2ae3b5b06
Neha Himesh <nehahimesh.10@gmail.com>
2026-03-03 13:57:46 +0530

    Created state variables in functional and class based components to show the difference

--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { useState } from "react";
 const User = (props) => {
+  const [count] = useState(0);
+  const [count2] = useState(1);
   return (
     <div className="user-card">
       <h2>Name: {props.name}</h2>
       <h3>Location: Bangalore</h3>
       <h4>Contact: neha@example.com</h4>
+      <h4> Count = {count}</h4>
+      <h4> Count2 = {count2}</h4>
     </div>
   );
 };
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,14 +3,21 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     console.log(props);
+    this.state = {
+      count: 0,
+      count2: 2,
+    };
   }
   render() {
     const { name, location } = this.props;
+    const { count, count2 } = this.state;
     return (
       <div className="user-card">
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: neha@example.com</h4>
+        <h5>Count: {count}</h5>
+        <h5>Count2: {count2} </h5>
       </div>
     );
   }
