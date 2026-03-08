3994d3c8c419f631a36adc85127e1cf6ea0be70b
Neha Himesh <nehahimesh.10@gmail.com>
2026-03-08 23:01:15 +0530

    Added restaurant menu details' jsx

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -6,9 +6,13 @@ const RestaurantMenu = () => {
 
   const resInfo = useRestaurantMenu(resId);
 
-  // const { name, cuisines, cloudinaryImageId, costForTwoMessage } =
-  //   resInfo?.data?.cards[2]?.card?.card?.info;
+  const { name, cuisines, cloudinaryImageId, costForTwoMessage } =
+    resInfo?.data?.cards[2]?.card?.card?.info;
   console.log(resInfo?.data?.cards[2]?.card?.card?.info);
+  const { itemCards } =
+    resInfo?.data?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+      ?.card;
+
   return resInfo === null ? (
     <Shimmer />
   ) : (
@@ -16,11 +20,17 @@ const RestaurantMenu = () => {
       {/* <h1>{name}</h1>
       <h3>{cuisines.join(", ")}</h3>
       <h3>{costForTwoMessage}</h3> */}
+      <h1>{name}</h1>
+      <p>
+        {cuisines.join(", ")} - {costForTwoMessage}
+      </p>
       <h2>Menu</h2>
       <ul>
-        <li>Biriyani</li>
-        <li>Burgers</li>
-        <li>Diet Coke</li>
+        {itemCards.map((item) => (
+          <li key={item.card.info.id}>
+            {item.card.info.name} - ₹{item.card.info.price / 100}
+          </li>
+        ))}
       </ul>
     </div>
   );
