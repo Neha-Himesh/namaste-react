58819ffbfba4d436c236774fe17e452d46218a2e
Neha Himesh <nehahimesh.10@gmail.com>
2026-03-12 16:48:32 +0530

    Tried to build accordion where when we click each title its subitems will be displayed

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,14 +1,25 @@
-const RestaurantCategory = (data) => {
-  console.log(data);
+const RestaurantCategory = ({ data }) => {
+  const [showItems, setShowItems] = useState(false);
+
+  const handleClick = () => {
+    setShowItems(!showItems);
+    // console.log(data);
+  };
+
   return (
     <div>
       <div className="w-6/12 m-auto my-5 bg-gray-50 shadow-lg p-4 flex justify-between">
-        <span className="font-bold">
-          {data.title} ({data.items.length})
-        </span>
-        <span>⬇️</span>
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={handleClick}
+        >
+          <span className="font-bold">
+            {data.title} ({data.items.length})
+          </span>
+          <span>⬇️</span>
+        </div>
       </div>
-      <ItemList items={data.itemCards} />
+      {showItems && <ItemList items={data.itemCards} />}
     </div>
   );
 };
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -19,29 +19,28 @@ const RestaurantMenu = () => {
         card.card?.card?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory",
     );
-  return resInfo === null ? (
-    <Shimmer />
-  ) : (
-    <div className="menu text-center">
-      {/* <h1>{name}</h1>
-      <h3>{cuisines.join(", ")}</h3>
-      <h3>{costForTwoMessage}</h3> */}
-      <h1 className="font-bold my-6 text-2xl">{name}</h1>
-      <p className="font-bold text-lg">
-        {cuisines.join(", ")} - {costForTwoMessage}
-      </p>
-      <h2>Menu</h2>
-      <ul>
-        {itemCards.map((item) => (
-          <li key={item.card.info.id}>
-            {item.card.info.name} - ₹{item.card.info.price / 100}
-          </li>
-        ))}
-      </ul>
-      {categories.map((category) => (
-        <RestaurantCategory data={category?.card?.card} />
-      ))}
-    </div>
-  );
+  return resInfo === null ? <Shimmer /> : <div>Res Category</div>;
+  // ) : (
+  //   <div className="menu text-center">
+  //     {/* <h1>{name}</h1>
+  //     <h3>{cuisines.join(", ")}</h3>
+  //     <h3>{costForTwoMessage}</h3> */}
+  //     <h1 className="font-bold my-6 text-2xl">{name}</h1>
+  //     <p className="font-bold text-lg">
+  //       {cuisines.join(", ")} - {costForTwoMessage}
+  //     </p>
+  //     <h2>Menu</h2>
+  //     <ul>
+  //       {itemCards.map((item) => (
+  //         <li key={item.card.info.id}>
+  //           {item.card.info.name} - ₹{item.card.info.price / 100}
+  //         </li>
+  //       ))}
+  //     </ul>
+  //     {categories.map((category) => (
+  //       <RestaurantCategory key={category?.card?.card?.title} data={category?.card?.card} />
+  //     ))}
+  //   </div>
+  // );
 };
 export default RestaurantMenu;
