9e59cb83f8a3f87f13682b8cf11ff9a847c959d8
Neha Himesh <nehahimesh.10@gmail.com>
2026-03-15 00:24:56 +0530

    Added Redux code to display cart items in the cart page and also clear the cart using clear cart button using Redux

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,14 +1,24 @@
 import { useDispatch } from "react-redux";
 const ItemList = ({ items }) => {
   const dispatch = useDispatch();
-  const handleAddItem = () => {
-    dispatch(addItem("pizza"));
+  const handleAddItem = (item) => {
+    dispatch(addItem(item));
   };
   return (
     <div>
-      <div className="">
-        <button onClick={handleAddItem}> Add Item </button>
-      </div>
+      {items.map((item) => (
+        <div
+          key={item.card.info.id}
+          className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between"
+        >
+          <div className="w-3/12 p-4">
+            <div className="absolute">
+              <button onClick={() => handleAddItem(item)}> Add Item </button>
+            </div>
+            <img src={CDN_URL + item.card.info.imageId} className="w-full" />
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
